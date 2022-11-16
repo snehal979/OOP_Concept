be9bfd300b868c9e0c71c2a2714b3c1d8832be81
Snehal979 <bansodsnehal535@gmail.com>
2022-11-16 16:53:45 +0530

    Main program for grain inventory

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace GrainInventoryJson
+{
+    class Program
+    {
+        public static string filepath = "C:\\Users\\hp\\Desktop\\NewBatch\\GrainInventoryJson\\GrainInventoryJson\\GrainDataFile.json";
+        public static void Main(string[] args)
+        {
+            ManageGrainFile manageGrainFile = new ManageGrainFile();
+            manageGrainFile.ReadFile(filepath);
+        }
+    }
+}
