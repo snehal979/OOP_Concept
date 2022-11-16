4f394bc442530b7d3b9c431ac7c596df9c09855b
Snehal979 <bansodsnehal535@gmail.com>
2022-11-16 16:59:55 +0530

    grain inventory problem

---
+++ GrainDataFile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrainInventoryJson
+{
+    public class GrainDataFile
+    {
+        public string Name { get; set; }
+        public int Weight { get; set; }
+        public double PricePerKg { get; set; }
+    }
+}
---
+++ ManageGrainFile.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace GrainInventoryJson
+{
+    public class ManageGrainFile
+    {
+        public void ReadFile(string filepath)
+        {
+            var json =File.ReadAllText(filepath);
+
+            List<GrainDataFile> grainDataFiles = JsonConvert.DeserializeObject<List<GrainDataFile>>(json); 
+            foreach( var data in grainDataFiles)
+            {
+                Console.WriteLine(data.Name+"\n"+data.Weight+"\n"+data.PricePerKg+"\n"+ "total ammount"+data.PricePerKg*data.Weight);
+            }
+        }
+    }
+}
